c87506587932ed0812168260849b5210e283684e
HungPham2002 <phamvinhhung02@gmail.com>
2022-10-26 12:43:30 +0700

    HideShow History & ListOfHistory

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
 import React, { useState } from 'react';
-import { Text, TextInput, View, ImageBackground, StyleSheet, TouchableOpacity as Button, Switch } from 'react-native';
+import { Text, TextInput, View, ImageBackground, StyleSheet, TouchableOpacity as Button, SafeAreaView, VirtualizedList, StatusBar } from 'react-native';
 import { Input } from 'react-native-elements';
 
+
 const image = { uri: "https://mobimg.b-cdn.net/v3/fetch/67/674d9f64c8a3c0110654ebdd1e037503.jpeg" };
-constructor() {
-  super();
-  this.state = {
-    content: true
-  }
-}
+// Content bên trong container history của các phép tính
+const DATA = [];
+const getItem = (data, index) => ({
+  id: Math.random().toString(12).substring(0),
+  title: `Math ${index+1}\nResult: ${index+1}`, // display các phép tính vào history
+});
+const getItemCount = (data) => 3; // count số lượng container history
+const Item = ({title}) => (
+  <View style={stylesList.item}>
+    <Text style={stylesList.title}>{title}</Text>
+  </View>
+);
+// Content bên trong container history của các phép tính
+
+
 const Calculator = () => {
   const [text, setText] = useState('');
-  const [isEnabled, setIsEnabled] = useState(false);
-  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+  const [shouldShow, setShouldShow] = useState(true); // hide & show component
   return (
     <ImageBackground source={image} resizeMode= 'cover' style = {styles.image}>
-    
+      
+{/* màn hình nhập phép tính và hiển thị kết quả */}
       <View style={{padding: 10, marginTop: 50}}>
         <View style = {{
           backgroundColor: 'rgba(204,255,204,1)',
@@ -33,6 +43,8 @@ const Calculator = () => {
           </Text>
         </View>
       </View>
+{/* màn hình nhập phép tính và hiển thị kết quả */}
+{/* các nút phép tính khoa học */}
       <View style ={{display: 'flex', flexDirection:'row', flexWrap: 'wrap', justifyContent: 'space-between'}}>
         <Button style={{
                             borderWidth:2,
@@ -156,31 +168,62 @@ const Calculator = () => {
             borderRadius:5,
             backgroundColor: 'gray',
             height: 70,
-            width: 60,
+            width: 80,
             alignItems: 'center',
             justifyContent: 'center'
           }}>
           <Text style = {{textAlign: 'center', fontSize: 18, color: 'white', fontWeight:'bold'}}>Calculate</Text>
         </Button>
       </View>
-      <View style = {{marginBottom: 20}}>
-          <Switch
-            trackColor={{ false: "#767577", true: "#81b0ff" }}
-            thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
-            ios_backgroundColor="#3e3e3e"
-            onValueChange={toggleSwitch}
-            value={isEnabled}
-          />
-      </View>
-      <View style = {{
-          backgroundColor: 'rgba(204,255,204,1)',
-          borderRadius: 15,
-          }}>
-        <Input label="HISTORY" placeholder='Search' />
-      </View>
+      {/* các nút phép tính khoa học */}
+      
+      {/* HISTORY */}
+
+      <SafeAreaView style = {{flex: 1}}>
+        <Button 
+          style = {{
+            borderWidth: 2,
+            borderColor:'white',
+            borderRadius:5,
+            backgroundColor: 'gray',
+            marginBottom: 20,
+            height: 50,
+            width: 120,
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}
+          onPress={() => setShouldShow(!shouldShow)}>
+          <Text style = {{textAlign: 'center', fontSize: 18, color: 'white', fontWeight:'bold'}}>Hide & Show History</Text>
+        </Button>
+        <View>
+          { shouldShow ? (
+            <View style = {{
+              backgroundColor: 'rgba(204,255,204,1)',
+              borderRadius: 15,
+              }}>
+              <Input label="HISTORY" placeholder='Search' />
+
+{/* list các container lịch sử các phép tính đã tính */}
+
+              <SafeAreaView style = {stylesList.container}>
+                <VirtualizedList
+                  data={DATA}
+                  initialNumToRender={4}
+                  renderItem={({ item }) => <Item title={item.title} />}
+                  keyExtractor={item => item.key}
+                  getItemCount = {getItemCount}
+                  getItem = {getItem}
+                />
+              </SafeAreaView>
+            </View>) : null
+          }
+        </View>
+      </SafeAreaView>
+        
     </ImageBackground>
   );
 };
+// Các thuộc tính của background
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -189,5 +232,26 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 });
+// Các thuộc tính của box history
+const stylesList = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+  item: {
+    backgroundColor: 'black',
+    borderRadius: 20,
+    height: 150,
+    justifyContent: 'center',
+    marginVertical: 8,
+    marginHorizontal: 16,
+    padding: 20,
+  },
+  title: {
+    fontSize: 32,
+    color: 'white',
+  },
+});
+
 
 export default Calculator;
