4a87e92f40d8b3593dda7e66de69c18cfd7d924d
anhdungbmt2001 <anhdungbmt2001@gmail.com>
2022-10-27 00:07:00 +0700

    Update UI & code indentation

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Text, TextInput, View, ImageBackground, StyleSheet, TouchableOpacity as Button, SafeAreaView, VirtualizedList, StatusBar } from 'react-native';
+import { Text, View, ImageBackground, StyleSheet, TouchableOpacity as Button, SafeAreaView, VirtualizedList, StatusBar } from 'react-native';
 import { Input } from 'react-native-elements';
 
 function log(num, base=10) {
@@ -20,8 +20,8 @@ const Item = ({title}) => (
   </View>
 );
 // Content bên trong container history của các phép tính
-
-
+  
+  
 const Calculator = () => {
   const [text, setText] = useState('');
   const [input, setInput] = useState('');
@@ -36,12 +36,12 @@ const Calculator = () => {
   return (
     <ImageBackground source={image} resizeMode= 'cover' style = {styles.image}>
     
-    {/* màn hình nhập phép tính và hiển thị kết quả */}
+      {/* màn hình nhập phép tính và hiển thị kết quả */}
       <View style={{padding: 10, marginTop: 50}}>
         <View style = {{
           backgroundColor: 'rgba(204,255,204,1)',
           borderRadius: 10,}}>
-          <TextInput
+          <Input
             ref={(thisObject) => { textInput = thisObject}}
             style={{height: 40, fontSize: 20,}}
             placeholder="What to calculate?"
@@ -55,151 +55,153 @@ const Calculator = () => {
           </Text>
         </View>
       </View>
-{/* màn hình nhập phép tính và hiển thị kết quả */}
-{/* các nút phép tính khoa học */}
+      
+      {/* màn hình nhập phép tính và hiển thị kết quả */}
+      {/* các nút phép tính khoa học */}
       <View style ={{display: 'flex', flexDirection:'row', flexWrap: 'wrap', justifyContent: 'space-between'}}>
         <Button style={{
-                            borderWidth:2,
-                            borderColor:'white',
-                            borderRadius: 5,
-                            backgroundColor: 'rgba(64,64,64,64)',
-                            marginBottom: 20,
-                            height: 50,
-                            alignItems: 'center',
-                            justifyContent: 'center'
-                            }}
-                onPress={() => {
-                  setInput(input + "√(");
-                  textInput.focus();
-                }}>
-            <Text style = {{textAlign: 'center', fontSize: 18, color: 'white'}}>√x      </Text>
+            borderWidth:2,
+            borderColor:'white',
+            borderRadius: 5,
+            backgroundColor: 'rgba(64,64,64,64)',
+            marginBottom: 20,
+            height: 50,
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}
+          onPress={() => {
+            setInput(input + "√(");
+            textInput.focus();
+          }}>
+          <Text style = {{textAlign: 'center', fontSize: 18, color: 'white'}}>√x</Text>
         </Button> 
         <Button style={{
-                            borderWidth:2,
-                            borderColor:'white',
-                            borderRadius: 5,
-                            backgroundColor: 'rgba(64,64,64,64)',
-                            height: 50,
-                            alignItems: 'center',
-                            justifyContent: 'center'
-                            }}
-                onPress={() => {
-                  setInput(input + "^2");
-                  textInput.focus();
-                }}>
-            <Text style = {{textAlign: 'center', fontSize: 18, color: 'white'}}>(x)^2</Text>
+            borderWidth:2,
+            borderColor:'white',
+            borderRadius: 5,
+            backgroundColor: 'rgba(64,64,64,64)',
+            height: 50,
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}
+          onPress={() => {
+            setInput(input + "^2");
+            textInput.focus();
+          }}>
+          <Text style = {{textAlign: 'center', fontSize: 18, color: 'white'}}>(x)^2</Text>
         </Button> 
         <Button style={{
-                            borderWidth:2,
-                            borderColor:'white',
-                            borderRadius: 5,
-                            backgroundColor: 'rgba(64,64,64,64)',
-                            height: 50,
-                            alignItems: 'center',
-                            justifyContent: 'center'
-                            }}
-                onPress={() => {
-                  setInput(input + "^");
-                  textInput.focus();
-                }}>
-            <Text style = {{textAlign: 'center', fontSize: 18, color: 'white'}}>(x)^y</Text>
+            borderWidth:2,
+            borderColor:'white',
+            borderRadius: 5,
+            backgroundColor: 'rgba(64,64,64,64)',
+            height: 50,
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}
+          onPress={() => {
+            setInput(input + "^");
+            textInput.focus();
+          }}>
+          <Text style = {{textAlign: 'center', fontSize: 18, color: 'white'}}>(x)^y</Text>
         </Button> 
         <Button style={{
-                            borderWidth:2,
-                            borderColor:'white',
-                            borderRadius: 5,
-                            backgroundColor: 'rgba(64,64,64,64)',
-                            height: 50,
-                            alignItems: 'center',
-                            justifyContent: 'center'
-                            }}
-                onPress={() => {
-                  setInput(input + "log(");
-                  textInput.focus();
-                }}>
-            <Text style = {{textAlign: 'center', fontSize: 18, color: 'white'}}>log(x,b)</Text>
+            borderWidth:2,
+            borderColor:'white',
+            borderRadius: 5,
+            backgroundColor: 'rgba(64,64,64,64)',
+            height: 50,
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}
+          onPress={() => {
+            setInput(input + "log(");
+            textInput.focus();
+          }}>
+          <Text style = {{textAlign: 'center', fontSize: 18, color: 'white'}}>log(x,b)</Text>
         </Button> 
         <Button style={{
-                            borderWidth:2,
-                            borderColor:'white',
-                            borderRadius: 5,
-                            backgroundColor: 'rgba(64,64,64,64)',
-                            height: 50,
-                            alignItems: 'center',
-                            justifyContent: 'center'
-                            }}
-                onPress={() => {
-                  setInput(input + "ln(");
-                  textInput.focus();
-                }}>
-            <Text style = {{textAlign: 'center', fontSize: 18, color: 'white'}}>ln(x)</Text>
+            borderWidth:2,
+            borderColor:'white',
+            borderRadius: 5,
+            backgroundColor: 'rgba(64,64,64,64)',
+            height: 50,
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}
+          onPress={() => {
+            setInput(input + "ln(");
+            textInput.focus();
+          }}>
+          <Text style = {{textAlign: 'center', fontSize: 18, color: 'white'}}>ln(x)</Text>
         </Button> 
         <Button style={{
-                            borderWidth:2,
-                            borderColor:'white',
-                            borderRadius: 5,
-                            backgroundColor: 'rgba(64,64,64,64)',
-                            height: 50,
-                            alignItems: 'center',
-                            justifyContent: 'center'
-                            }}
-                onPress={() => {
-                  setInput(input + "sin(");
-                  textInput.focus();
-                }}>
-            <Text style = {{textAlign: 'center', fontSize: 18, color: 'white'}}>sin(x)</Text>
+            borderWidth:2,
+            borderColor:'white',
+            borderRadius: 5,
+            backgroundColor: 'rgba(64,64,64,64)',
+            height: 50,
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}
+          onPress={() => {
+            setInput(input + "sin(");
+            textInput.focus();
+          }}>
+          <Text style = {{textAlign: 'center', fontSize: 18, color: 'white'}}>sin(x)</Text>
         </Button> 
         <Button style={{
-                            borderWidth:2,
-                            borderColor:'white',
-                            borderRadius: 5,
-                            backgroundColor: 'rgba(64,64,64,64)',
-                            height: 50,
-                            alignItems: 'center',
-                            justifyContent: 'center'
-                            }}
-                onPress={() => {
-                  setInput(input + "cos(");
-                  textInput.focus();
-                }}>
-            <Text style = {{textAlign: 'center', fontSize: 18, color: 'white'}}>cos(x)</Text>
+            borderWidth:2,
+            borderColor:'white',
+            borderRadius: 5,
+            backgroundColor: 'rgba(64,64,64,64)',
+            height: 50,
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}
+          onPress={() => {
+            setInput(input + "cos(");
+            textInput.focus();
+          }}>
+          <Text style = {{textAlign: 'center', fontSize: 18, color: 'white'}}>cos(x)</Text>
         </Button> 
         <Button style={{
-                            borderWidth:2,
-                            borderColor:'white',
-                            borderRadius: 5,
-                            marginBottom: 20,
-                            marginRight: 230,
-                            backgroundColor: 'rgba(64,64,64,64)',
-                            height: 50,
-                            alignItems: 'center',
-                            justifyContent: 'center'
-                            }}
-                onPress={() => {
-                  setInput(input + "tan(");
-                  textInput.focus();
-                }}>
-            <Text style = {{textAlign: 'center', fontSize: 18, color: 'white'}}>tan(x)</Text>
-        </Button> 
-        <Button style={{
-                            borderWidth:2,
-                            borderColor:'white',
-                            borderRadius: 5,
-                            backgroundColor: 'gray',
-                            height: 70,
-                            width: 60,
-                            alignItems: 'center',
-                            justifyContent: 'center'
-                            }}
-                onPress={() => {
-                  setInput("");
-                  setText("");
-                  textInput.focus();
-                }}>
-            <Text style = {{textAlign: 'center', fontSize: 18, color: 'white', fontWeight:'bold'}}>clear</Text>
+            borderWidth:2,
+            borderColor:'white',
+            borderRadius: 5,
+            marginBottom: 20,
+            marginRight: 230,
+            backgroundColor: 'rgba(64,64,64,64)',
+            height: 50,
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}
+          onPress={() => {
+            setInput(input + "tan(");
+            textInput.focus();
+          }}>
+          <Text style = {{textAlign: 'center', fontSize: 18, color: 'white'}}>tan(x)</Text>
         </Button> 
       </View>
-      <View style={{padding: 10, alignItems: 'center', marginTop: 20}}>
+      <View style={{justifyContent: 'center', marginTop: 20, display: 'flex', flexDirection:'row', flexWrap: 'wrap'}}>
+        <Button style={{
+            borderWidth: 2,
+            borderColor:'white',
+            borderRadius: 5,
+            backgroundColor: 'gray',
+            height: 50,
+            width: 60,
+            alignItems: 'center',
+            marginHorizontal: 10,
+            justifyContent: 'center'
+          }}
+          onPress={() => {
+            setInput("");
+            setText("");
+            textInput.focus();
+          }}>
+          <Text style = {{textAlign: 'center', fontSize: 18, color: 'white', fontWeight:'bold'}}>clear</Text>
+        </Button> 
         <Button style = {{
             borderWidth: 2,
             borderColor:'white',
@@ -208,6 +210,7 @@ const Calculator = () => {
             height: 50,
             width: 100,
             alignItems: 'center',
+            marginHorizontal: 10,
             justifyContent: 'center'
           }}
           onPress={() => {
@@ -226,17 +229,18 @@ const Calculator = () => {
       {/* các nút phép tính khoa học */}
       
       {/* HISTORY */}
-
-      <SafeAreaView style = {{flex: 1}}>
+      
+      <SafeAreaView style = {{flex: 1, alignItems: 'center'}}>
         <Button 
           style = {{
             borderWidth: 2,
             borderColor:'white',
             borderRadius:5,
             backgroundColor: 'gray',
+            marginTop: 50,
             marginBottom: 20,
             height: 50,
-            width: 120,
+            width: 200,
             alignItems: 'center',
             justifyContent: 'center'
           }}
@@ -244,62 +248,62 @@ const Calculator = () => {
           <Text style = {{textAlign: 'center', fontSize: 18, color: 'white', fontWeight:'bold'}}>Hide & Show History</Text>
         </Button>
         <View>
-          { shouldShow ? (
-            <View style = {{
+        { shouldShow ? (
+          <View style = {{
               backgroundColor: 'rgba(204,255,204,1)',
               borderRadius: 15,
-              }}>
-              <Input label="HISTORY" placeholder='Search' />
-
-{/* list các container lịch sử các phép tính đã tính */}
-
-              <SafeAreaView style = {stylesList.container}>
-                <VirtualizedList
-                  data={DATA}
-                  initialNumToRender={4}
-                  renderItem={({ item }) => <Item title={item.title} />}
-                  keyExtractor={item => item.key}
-                  getItemCount = {getItemCount}
-                  getItem = {getItem}
-                />
-              </SafeAreaView>
-            </View>) : null
-          }
+            }}>
+            <Input label="HISTORY" placeholder='Search' />
+            
+            {/* list các container lịch sử các phép tính đã tính */}
+            
+            <SafeAreaView style = {stylesList.container}>
+              <VirtualizedList
+                data={DATA}
+                initialNumToRender={4}
+                renderItem={({ item }) => <Item title={item.title} />}
+                keyExtractor={item => item.key}
+                getItemCount = {getItemCount}
+                getItem = {getItem}
+              />
+            </SafeAreaView>
+          </View>) : null
+        }
         </View>
       </SafeAreaView>
-        
+      
     </ImageBackground>
-  );
-};
-// Các thuộc tính của background
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  image: {
-    flex: 1,
-  },
-});
-// Các thuộc tính của box history
-const stylesList = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  item: {
-    backgroundColor: 'black',
-    borderRadius: 20,
-    height: 150,
-    justifyContent: 'center',
-    marginVertical: 8,
-    marginHorizontal: 16,
-    padding: 20,
-  },
-  title: {
-    fontSize: 32,
-    color: 'white',
-  },
-});
-
-
-export default Calculator;
+    );
+  };
+  // Các thuộc tính của background
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    image: {
+      flex: 1,
+    },
+  });
+  // Các thuộc tính của box history
+  const stylesList = StyleSheet.create({
+    container: {
+      flex: 1,
+      marginTop: StatusBar.currentHeight,
+    },
+    item: {
+      backgroundColor: 'black',
+      borderRadius: 20,
+      height: 150,
+      justifyContent: 'center',
+      marginVertical: 8,
+      marginHorizontal: 16,
+      padding: 20,
+    },
+    title: {
+      fontSize: 32,
+      color: 'white',
+    },
+  });
+  
+  
+  export default Calculator;
